eeb92319ca3b4e3d1a8d272d4e37442ac4208635
Patrik Simandan <patrik.simandan@student.upt.ro>
2024-11-08 23:27:40 +0200

    Home-page map stuff

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import OSM from 'ol/source/OSM';
+import { useEffect } from "react";
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate();
@@ -9,9 +15,29 @@ const HomePage: React.FC = () => {
         navigate("/login"); // Redirecționează către pagina de login
     };
 
+    const mapContainerStyle = { width: '100%', height: '1000px', border: '1px solid black' };
+
+    useEffect(() => {
+        const osmSource = new OSM();
+
+        new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: osmSource,
+                }),
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 2,
+            }),
+        });
+    }, []);
+
     return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <div style={{textAlign: "center", marginTop: "50px"}}>
             <h1>Welcome to the Home Page!</h1>
+            <div id='map' style={mapContainerStyle}></div>
             <button onClick={handleLogout}>Logout</button>
         </div>
     );
