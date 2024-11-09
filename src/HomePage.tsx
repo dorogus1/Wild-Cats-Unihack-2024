c75591e06c882f874d868f80f0916b2f33a864b3
ameliaPaizs <amelia.paizs@student.upt.ro.com>
2024-11-09 14:23:26 +0200

    pinpoint the search location on the map

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import { useNavigate } from "react-router-dom";
 import Map from 'ol/Map';
 import View from 'ol/View';
@@ -9,11 +9,17 @@ import { fromLonLat } from 'ol/proj';
 import "ol/ol.css"
 import "ol-ext/dist/ol-ext.css"
 import SearchNominatim from "ol-ext/control/SearchNominatim";
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
 
 import LayerSwitcher from "ol-ext/control/LayerSwitcher"
+import VectorLayer from "ol/layer/Vector";
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate();
+    const mapRef = useRef<Map | null>(null);
 
     const handleLogout = () => {
         localStorage.removeItem("isLoggedIn");
@@ -28,18 +34,25 @@ const HomePage: React.FC = () => {
         });
         const timisoaraCoordinates = fromLonLat([21.2087, 45.7489]);
 
+        const vectorSource = new VectorSource();
+        const vectorLayer = new VectorLayer({
+            source: vectorSource,
+        });
+
         const map = new Map({
             target: 'map',
             layers: [
                 new TileLayer({
                     source: xyzSource,
                 }),
+                vectorLayer,
             ],
             view: new View({
                 center: timisoaraCoordinates,
                 zoom: 13,
             }),
         });
+        mapRef.current = map;
 
         const layerSwitcher = new LayerSwitcher();
         map.addControl(layerSwitcher);
@@ -54,6 +67,27 @@ const HomePage: React.FC = () => {
 
         searchNominatim.on('select', (e) => {
             const coordinate = e.coordinate;
+
+            const pointFeature = new Feature({
+                geometry: new Point(coordinate)
+            });
+
+            pointFeature.setStyle(new Style({
+                image: new CircleStyle({
+                    radius: 7,
+                    fill: new Fill({
+                        color: 'red'
+                    }),
+                    stroke: new Stroke({
+                        color: 'white',
+                        width: 2
+                    })
+                })
+            }));
+
+            vectorSource.clear(); // Curăță marker-ele existente dacă dorești
+            vectorSource.addFeature(pointFeature);
+
             const view = map.getView();
             view.setCenter(coordinate);
             view.setZoom(14);
