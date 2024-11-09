1f9e2a3cb0a0169a4f717f938ce613c2fcfa84a0
Patrik Simandan <patrik.simandan@student.upt.ro>
2024-11-09 12:58:22 +0200

    Added search

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -6,13 +6,18 @@ import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import { useEffect } from "react";
 import { fromLonLat } from 'ol/proj';
+import "ol/ol.css"
+import "ol-ext/dist/ol-ext.css"
+import SearchNominatim from "ol-ext/control/SearchNominatim";
+
+import LayerSwitcher from "ol-ext/control/LayerSwitcher"
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        localStorage.removeItem("isLoggedIn"); // Șterge autentificarea din localStorage
-        navigate("/login"); // Redirecționează către pagina de login
+        localStorage.removeItem("isLoggedIn");
+        navigate("/login");
     };
 
     const mapContainerStyle = { width: '100%', height: '1000px', border: '1px solid black' };
@@ -23,7 +28,7 @@ const HomePage: React.FC = () => {
         });
         const timisoaraCoordinates = fromLonLat([21.2087, 45.7489]);
 
-        new Map({
+        const map = new Map({
             target: 'map',
             layers: [
                 new TileLayer({
@@ -35,12 +40,30 @@ const HomePage: React.FC = () => {
                 zoom: 13,
             }),
         });
+
+        const layerSwitcher = new LayerSwitcher();
+        map.addControl(layerSwitcher);
+
+        const searchNominatim = new SearchNominatim({
+            reverse: false,
+            placeholder: 'Search for location...',
+            maxItems: 10,
+            minLength: 3,
+        });
+        map.addControl(searchNominatim);
+
+        searchNominatim.on('select', (e) => {
+            const coordinate = e.coordinate;
+            const view = map.getView();
+            view.setCenter(coordinate);
+            view.setZoom(14);
+        });
     }, []);
 
     return (
-        <div style={{textAlign: "center", marginTop: "50px"}}>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
             <h1>Welcome to the Home Page!</h1>
-            <div id='map' style={mapContainerStyle}></div>
+            <div id="map" style={mapContainerStyle}></div>
             <button onClick={handleLogout}>Logout</button>
         </div>
     );
